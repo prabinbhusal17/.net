5bc4f47f06c4d41b07b8708509f1e45535d0166b
prabinbhusal0307 <praveenvhusal897@gmail.com>
2026-07-15 17:44:30 +0545

    Added asyncawaitexample

---
+++ DotNetAssignment/ASynchronous await/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Program Started");
+
+        await DownloadData();
+
+        Console.WriteLine("Program Finished");
+    }
+
+    static async Task DownloadData()
+    {
+        Console.WriteLine("Downloading data...");
+
+        await Task.Delay(3000);
+
+        Console.WriteLine("Download Complete");
+    }
+}
