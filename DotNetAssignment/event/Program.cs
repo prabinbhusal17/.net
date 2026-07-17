7ed8598ffb444b155dfef43c13e9e4a8f12d54f0
prabinbhusal0307 <praveenvhusal897@gmail.com>
2026-07-17 09:43:02 +0545

    Added event example

---
+++ DotNetAssignment/event/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Button
+{
+    // Declare an event
+    public event EventHandler ButtonClicked;
+
+    public void Click()
+    {
+        Console.WriteLine("Button clicked.");
+        ButtonClicked?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Button button = new Button();
+
+        // Subscribe to the event
+        button.ButtonClicked += OnButtonClicked;
+
+        // Raise the event
+        button.Click();
+
+        Console.ReadLine();
+    }
+
+    static void OnButtonClicked(object sender, EventArgs e)
+    {
+        Console.WriteLine("Event handled successfully.");
+    }
+}
